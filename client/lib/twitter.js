234b03c6f9222a8cf868bd4c5fbd1a11844e3698
Mikel <amilburu@elipse.it>
2014-11-08 16:42:25 +0100

    User connecion handling

--- client/lib/twitter.js
+++ client/lib/twitter.js
@@ -4,9 +4,7 @@
     //console.log(results); //results.data should be a JSON object
   //});
 
- Meteor.call('twitsByLocation', 1, function(e, r) {
- 		console.log('chiamato');
-      });
+
 
 Meteor.subscribe('tweets');
 
@@ -15,4 +13,23 @@ Template.private.helpers({
     'tweets': function(){
         return Tweets.find({}, {sort: {"creationDate": -1}});
     }
+});
+
+// client code: ping heartbeat every 5 seconds
+Meteor.setInterval(function () {
+	console.log(Meteor.userId());
+  Meteor.call('keepalive', Meteor.userId());
+}, 5000);
+
+Template.nav.events({
+'click #private': function (event) {
+ Meteor.call('twitsByLocation', Meteor.userId(), function(e, r) {
+ 		console.log('chiamato');
+      });
+},
+'click #home': function (event) {
+ Meteor.call('kill', 1, function(e, r) {
+ 		console.log('killato');
+      });
+}
 });
--- server/lib/config/twitter.js
+++ server/lib/config/twitter.js
@@ -2,11 +2,13 @@
         return Tweets.find({}, {sort: {"creationDate": -1}, limit: 20});
     });
 
+
     Meteor.startup(function () {
     // code to run on server at startup
-    var wrappedInsert = Meteor.bindEnvironment(function(tweet) {
-    Tweets.insert({"username": tweet.user.screen_name , "userTweet" : tweet.text, "profilePhoto": tweet.user.profile_image_url, "creationDate" : tweet.created_at});
+    var wrappedInsert = Meteor.bindEnvironment(function(tweet, user_id) {
+    Tweets.insert({"username": tweet.user.screen_name , "userTweet" : tweet.text, "profilePhoto": tweet.user.profile_image_url, "creationDate" : tweet.created_at, "user_id" : user_id});
   }, "Failed to insert tweet into Posts collection.");
+    Connections.remove({});
 
    
 
@@ -16,31 +18,63 @@
 
      Meteor.methods({
 
-      twitsByLocation:  function(arg){
-        var Twit = Meteor.npmRequire('twit');
-        var conf = JSON.parse(Assets.getText('api_keys.json'));
-        var T = new Twit({
-            consumer_key: conf.consumer_key,
-            consumer_secret: conf.consumer_secret,
-            access_token: conf.access_token,
-            access_token_secret: conf.access_token_secret
-        });
+      twitsByLocation:  function(user_id){
+            console.log(user_id);
+            console.log(Connections.findOne({user_id : user_id}));
+            if (!Connections.findOne({user_id : user_id})){
+                Connections.insert({user_id: user_id});
+                console.log('inserito');
+                console.log('entrato');
+                var Twit = Meteor.npmRequire('twit');
+                var conf = JSON.parse(Assets.getText('api_keys.json'));
+                var T = new Twit({
+                    consumer_key: conf.consumer_key,
+                    consumer_secret: conf.consumer_secret,
+                    access_token: conf.access_token,
+                    access_token_secret: conf.access_token_secret
+            });
 
-        var city_area = [  '-122.75', '36.8', '-121.75', '37.8' ]
+            var city_area = [  '-122.75', '36.8', '-121.75', '37.8' ]
 
-        stream = T.stream('statuses/filter', { locations: city_area });
+            stream = T.stream('statuses/filter', { locations: city_area });
 
-        stream.on('tweet', function (tweet) {
-            userName = tweet.user.screen_name;
-            userTweet = tweet.text;
-            creationDate = tweet.created_at;
-            wrappedInsert(tweet);
-            console.log(userName+" says "+userTweet+" at "+ creationDate);
+            stream.on('tweet', function (tweet, user_id) {
+                userName = tweet.user.screen_name;
+                userTweet = tweet.text;
+                creationDate = tweet.created_at;
+                wrappedInsert(tweet, user_id);
+                console.log(userName+" says "+userTweet+" at "+ creationDate);
 
             
-        });
+            });
+        }
         
-      }
-    });
+    },
+
+      // server code: heartbeat method
+
+    keepalive: function (user_id) {
+    if (Connections.findOne({user_id : user_id})){
+        Connections.update(user_id, {$set: {last_seen: (new Date()).getTime()}});
+        }
+    },
+    kill: function (user_id){
+        if (Connections.findOne(user_id)){
+            Connections.delete({user_id: user_id});
+        }
+    }
+
+
+
+});
+
+});
+
+  // server code: clean up dead clients after 60 seconds
+Meteor.setInterval(function () {
+  var now = (new Date()).getTime();
+  Connections.find({last_seen: {$lt: (now - 60 * 1000)}}).forEach(function (user) {
+    // do something here for each idle user
+  });
 }); 
 
